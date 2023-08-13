5f3de49e029ad0d523644b5443ff8f948eb29092
Eylon Shmilovich <eylonshm@gmail.com>
2023-08-13 20:14:01 +0300

    Fetch Mock Data

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import styles from './index.module.scss'
 import { Link } from 'react-router-dom'
+import { ROUTES } from '../../constants'
 
 const Tasks = () => {
   return (
     <div className={styles.wrapper}>
-      <Link className={styles.link} to={'/tasks'}>
+      <Link className={styles.link} to={ROUTES.tasks}>
         That's just an empty home screen, Click here to view tasks
       </Link>
     </div>
--- src/routes/Tasks/index.tsx
+++ src/routes/Tasks/index.tsx
@@ -1,13 +1,32 @@
-import React, { useCallback } from 'react'
+//@ts-nocheck
+
+import React, { useCallback, useState, useEffect } from 'react'
 import styles from './index.module.scss'
 import copiesPrefix from '../../copies.json'
 import Button from '../../components/Button'
 import Task from '../../components/Task'
 import { TASKS } from '../../mock/data'
+import { Rings } from 'react-loader-spinner'
 
 const copies = copiesPrefix.tasks
 
 const Tasks = () => {
+  const [tasks, setTasks] = useState<[]>([])
+
+  const loadingSpinner = () => {
+    return (
+      <Rings
+        height='80'
+        width='80'
+        color='#eaae13'
+        radius='6'
+        wrapperStyle={{}}
+        wrapperClass=''
+        visible={true}
+        ariaLabel='rings-loading'
+      />
+    )
+  }
   const renderTasks = useCallback(() => {
     return TASKS.map(
       ({
@@ -36,13 +55,22 @@ const Tasks = () => {
     )
   }, [TASKS])
 
+  useEffect(() => {
+    // API CALL SHOULD BE HERE
+    setTimeout(() => {
+      setTasks(TASKS)
+    }, 1500)
+  }, [])
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.top}>
         <h3 className={styles.title}>{copies.header}</h3>
         <Button text={copies.addTask} onClick={() => {}} />
       </div>
-      <div className={styles.tasks}>{renderTasks()} </div>
+      <div className={styles.tasks}>
+        {tasks.length === 0 ? loadingSpinner() : renderTasks()}
+      </div>
     </div>
   )
 }
