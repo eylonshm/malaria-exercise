b67bff099a2ddc01942978dbf4827ca706517e3d
Eylon Shmilovich <eylonshm@gmail.com>
2023-08-13 19:59:12 +0300

    Add Link to tasks page inside HomeScreen

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import styles from './index.module.scss'
+import { Link } from 'react-router-dom'
 
 const Tasks = () => {
-  return <div>Home</div>
+  return (
+    <div className={styles.wrapper}>
+      <Link className={styles.link} to={'/tasks'}>
+        That's just an empty home screen, Click here to view tasks
+      </Link>
+    </div>
+  )
 }
 
 export default Tasks
