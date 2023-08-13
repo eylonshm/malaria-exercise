026d5952b86aa2aa54eb444dac96f38bbaa6b283
Eylon Shmilovich <eylonshm@gmail.com>
2023-08-13 15:15:58 +0300

    Initial React router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Home, Tasks } from './routes'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { ROUTES } from './constants'
+
+const router = createBrowserRouter([
+  {
+    path: ROUTES.root,
+    element: <Home />,
+  },
+  {
+    path: ROUTES.tasks,
+    element: <Tasks />,
+  },
+])
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <RouterProvider router={router} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
